9bd2c47baf0d05bc00a686c58838ea8a8c3066fb
Yang Gu <yang.gu@intel.com>
2021-08-23 16:37:35 +0800

    Fix unit test on shwde7700 using git bash

--- src/unit.js
+++ src/unit.js
@@ -23,13 +23,13 @@ async function runUnit() {
     let timeout;
     if (util.dryrun) {
       cmd = 'yarn test --grep nextFrame';
-      timeout = 60 * 1000;
+      timeout = 120 * 1000;
     } else {
       cmd = 'yarn test';
       timeout = 600 * 1000;
     }
 
-    const logFile = path.join(util.outDir, `${util.timestamp}-unit-${backend}.txt`);
+    let logFile;
     let tfjsDir = '';
     if ('tfjs-dir' in util.args) {
       tfjsDir = util.args['tfjs-dir'];
@@ -38,7 +38,13 @@ async function runUnit() {
     }
     process.chdir(path.join(tfjsDir, `tfjs-backend-${backend}`));
     process.env['CHROME_BIN'] = util.browserPath;
-    spawnSync('cmd', ['/c', `${cmd} > ${logFile}`], {env: process.env, stdio: [process.stdin, process.stdout, process.stderr], timeout: timeout});
+    if (util.hostname == 'shwde7700') {
+      logFile = path.join(util.outDir, `${util.timestamp}-unit-${backend}.txt`).replace('\\', '/').replace(/\\/g, '/');
+      spawnSync('C:/Program Files/Git/git-bash.exe', ['-c', `${cmd} > ${logFile}`], {env: process.env, stdio: [process.stdin, process.stdout, process.stderr], timeout: timeout});
+    } else {
+      logFile = path.join(util.outDir, `${util.timestamp}-unit-${backend}.txt`);
+      spawnSync('cmd', ['/c', `${cmd} > ${logFile}`], {env: process.env, stdio: [process.stdin, process.stdout, process.stderr], timeout: timeout});
+    }
     var lines = fs.readFileSync(logFile, 'utf-8').split('\n').filter(Boolean);
     for (let line of lines) {
       if (line.includes('FAILED') || line.includes('Executed')) {
