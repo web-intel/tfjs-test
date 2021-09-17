899ebc208550ab7e0e4b88ff2230c39c427ef55a
Yang Gu <yang.gu@intel.com>
2021-09-17 12:25:34 +0800

    Use WGSL by default

--- src/util.js
+++ src/util.js
@@ -41,7 +41,7 @@ module.exports = {
   'targetMetrics': targetMetrics,
   'outDir': outDir,
   'url': 'http://wp-27.sh.intel.com/workspace/project/tfjswebgpu/tfjs/e2e/benchmarks/local-benchmark/',
-  'urlArgs': 'localBuild=webgl,webgpu&WEBGL_USE_SHAPES_UNIFORMS=true&WEBGPU_USE_GLSL=false',
+  'urlArgs': 'localBuild=webgl,webgpu&WEBGL_USE_SHAPES_UNIFORMS=true',
   'timeout': 180 * 1000,
   log: log,
 };
